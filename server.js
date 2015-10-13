7653ffa235e83f6aec1bf69e76e0a88ba8c1384e
Ronald Ishak <ronald@ishak.net>
2015-10-13 17:19:52 +0700

    updated for heroku

--- server.js
+++ server.js
@@ -79,9 +79,7 @@ app.delete('/items/:id', function(req, res) {
   }
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
